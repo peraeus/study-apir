036629b459a498717a06c45fef692e73fa2b135c
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-09 22:45:09 -0300

    att dependencies

--- src/main/java/br/com/fiap/study_apir/controller/PingController.java
+++ src/main/java/br/com/fiap/study_apir/controller/PingController.java
@@ -1,4 +1,4 @@
-package br.com.fiap.study_apir;
+package br.com.fiap.study_apir.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +14,21 @@ public class PingController {
 
     @GetMapping("/ping")
     public String ping() {
-        return "pong";
+        return "pong blz";
     }
 
     @GetMapping("/rota1")
     public String rota1() {
         return "rota1";
+    }
+
+    @GetMapping("/rota2")
+    public String rota2() {
+        return "rota2"; // resposta padrao
+    }
+
+    @GetMapping("/rota3")
+    public String rota3() {
+        return "rota3"; // resposta padrao
     }
 }
