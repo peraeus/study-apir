e8285575ef6b70cc49e84bc94f17bfb2fdde96fb
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-23 22:46:17 -0300

    feat: @PathVariable

--- src/main/java/br/com/fiap/study_apir/controller/ProdutoController.java
+++ src/main/java/br/com/fiap/study_apir/controller/ProdutoController.java
@@ -1,10 +1,13 @@
 package br.com.fiap.study_apir.controller;
 
+import java.util.List;
+
 import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,10 +27,15 @@ public class ProdutoController {
         return ResponseEntity.status(HttpStatus.CREATED).body("Produto criado");
     }
 
-    @GetMapping
-    public ResponseEntity<Produto> findById(Long id){
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id){
         Produto produto = mockup.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(produto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(mockup.findAll());
     }
 
     @PutMapping
