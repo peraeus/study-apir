566be232525d5e432c6b1693297f01ec5c6b7fe2
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-11 22:39:08 -0300

    dto e service

--- src/main/java/br/com/fiap/study_apir/model/Produto.java
+++ src/main/java/br/com/fiap/study_apir/model/Produto.java
@@ -3,6 +3,8 @@ package br.com.fiap.study_apir.model;
 import java.math.BigDecimal;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -21,15 +23,17 @@ import lombok.Data;
 public class Produto {
     public Produto() {
     }
-    
+
     public Produto(Long id, String nome, BigDecimal valor) {
         this.id = id;
         this.nome = nome;
         this.valor = valor;
 
     }
+
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String nome;
     private BigDecimal valor; // para valores monetários
-    }
+}
